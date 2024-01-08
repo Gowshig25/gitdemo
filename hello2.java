9e25c19f7187dbd663bcb2b4c49ac381740f5b10
Gowshig25 <gowshigansaraswathi@gmail.com>
2024-01-08 19:20:05 +0530

    with change

--- hello2.java
+++ hello2.java
@@ -1,3 +1,8 @@
+import java.lang.*;
+import java.util.*;
 public class hello2 {
-    
+    public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    System.out.println("hello world!");
+    }
 }
